a1e41136a85e6759e08c4c71c1678e86fbf6a343
Adam Johnson <adam@adamjohnson.org>
2024-10-21 21:33:56 +1100

    Update chat.tsx

--- app/chat.tsx
+++ app/chat.tsx
@@ -1,15 +1,30 @@
 import { StyleSheet, ScrollView, SafeAreaView, KeyboardAvoidingView, Platform } from "react-native";
-import { Stack } from "expo-router";
-import { useHeaderHeight } from "@react-navigation/elements";
+import { Stack, useRouter } from "expo-router";
+import { HeaderBackButton, useHeaderHeight } from "@react-navigation/elements";
 import Response from "components/chat/Response";
 import Input from "components/chat/Input";
+import { theme } from "utils/helpers";
 
 export default function Chat() {
   const headerHeight = useHeaderHeight();
+  const router = useRouter();
+  const colors = theme();
 
   return (
     <SafeAreaView style={styles.container}>
-      <Stack.Screen options={{ title: "", headerBackTitle: "Back" }} />
+      <Stack.Screen
+        options={{
+          headerTitle: "",
+          headerLeft: () => (
+            <HeaderBackButton
+              onPress={() => router.dismissAll()}
+              labelStyle={{ fontFamily: "Circular-Book" }}
+              tintColor={colors.primary}
+              allowFontScaling={false}
+            />
+          ),
+        }}
+      />
 
       <KeyboardAvoidingView
         style={styles.container}
