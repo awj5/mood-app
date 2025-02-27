03b2f1bf6e8c13c71d11c35aea53378c57ffaa53
Adam Johnson <adam@adamjohnson.org>
2025-02-27 11:12:36 +1000

    Update Disclaimer.tsx

--- components/company/Disclaimer.tsx
+++ components/company/Disclaimer.tsx
@@ -1,6 +1,6 @@
 import { StyleSheet, View, Text, Pressable } from "react-native";
 import * as Device from "expo-device";
-import { ChartSpline } from "lucide-react-native";
+import { ChartSpline, ShieldCheck } from "lucide-react-native";
 import Button from "components/Button";
 import { theme, pressedDefault } from "utils/helpers";
 
@@ -12,6 +12,7 @@ export default function Disclaimer(props: DisclaimerProps) {
   const colors = theme();
   const spacing = Device.deviceType !== 1 ? 24 : 16;
   const fontSize = Device.deviceType !== 1 ? 20 : 16;
+  const fontSizeSmall = Device.deviceType !== 1 ? 18 : 14;
 
   const agree = () => {
     alert("Coming soon");
@@ -24,7 +25,6 @@ export default function Disclaimer(props: DisclaimerProps) {
         {
           paddingHorizontal: spacing * 1.5,
           gap: spacing * 2,
-          backgroundColor: colors.secondaryBg,
         },
       ]}
     >
@@ -62,11 +62,61 @@ export default function Disclaimer(props: DisclaimerProps) {
           To view real-time insights into workplace wellbeing trends at{" "}
           <Text style={{ fontFamily: "Circular-Bold" }}>{props.company}</Text>, you must first agree to anonymously
           share your check-ins. This does not include any information shared in your private chats with{" "}
-          <Text style={{ fontFamily: "Circular-Bold" }}>MOOD</Text>.{"\n\n"}
-          <Text style={{ fontFamily: "Circular-Bold" }}>
-            Neither {props.company} nor MOOD.ai are able to identify an individual user's check-in.
-          </Text>
+          <Text style={{ fontFamily: "Circular-Bold" }}>MOOD</Text>.
         </Text>
+      </View>
+
+      <View style={{ backgroundColor: colors.secondaryBg, padding: spacing, borderRadius: spacing, gap: spacing }}>
+        <View style={[styles.heading, { gap: Device.deviceType !== 1 ? 10 : 6 }]}>
+          <ShieldCheck
+            color={colors.primary}
+            size={Device.deviceType !== 1 ? 28 : 20}
+            absoluteStrokeWidth
+            strokeWidth={Device.deviceType !== 1 ? 2 : 1.5}
+          />
+
+          <Text
+            style={{
+              fontFamily: "Circular-Bold",
+              color: colors.primary,
+              fontSize: fontSizeSmall,
+            }}
+            allowFontScaling={false}
+          >
+            PRIVACY PROTECTED
+          </Text>
+        </View>
+
+        <Text
+          style={[
+            styles.text,
+            {
+              color: colors.primary,
+              fontSize: fontSize,
+            },
+          ]}
+          allowFontScaling={false}
+        >
+          Neither <Text style={{ fontFamily: "Circular-Bold" }}>{props.company}</Text> nor{" "}
+          <Text style={{ fontFamily: "Circular-Bold" }}>MOOD.ai</Text> are able to identify an individual user's
+          check-in.
+        </Text>
+
+        <Pressable onPress={() => alert("Coming soon")} style={({ pressed }) => pressedDefault(pressed)} hitSlop={16}>
+          <Text
+            style={[
+              styles.text,
+              {
+                color: colors.secondary,
+                fontSize: fontSizeSmall,
+                textDecorationLine: "underline",
+              },
+            ]}
+            allowFontScaling={false}
+          >
+            Learn more about our{"\n"}commitment to your privacy
+          </Text>
+        </Pressable>
       </View>
 
       <View style={{ alignItems: "center" }}>
@@ -74,22 +124,6 @@ export default function Disclaimer(props: DisclaimerProps) {
           Agree and continue
         </Button>
       </View>
-
-      <Pressable onPress={() => alert("Coming soon")} style={({ pressed }) => pressedDefault(pressed)} hitSlop={16}>
-        <Text
-          style={[
-            styles.text,
-            {
-              color: colors.secondary,
-              fontSize: fontSize,
-              textDecorationLine: "underline",
-            },
-          ]}
-          allowFontScaling={false}
-        >
-          Learn more about our{"\n"}commitment to your privacy
-        </Text>
-      </Pressable>
     </View>
   );
 }
@@ -111,5 +145,10 @@ const styles = StyleSheet.create({
   text: {
     fontFamily: "Circular-Book",
     textAlign: "center",
+  },
+  heading: {
+    flexDirection: "row",
+    alignItems: "center",
+    alignSelf: "center",
   },
 });
