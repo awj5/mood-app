502fcaff949e0207ae00be31e0cfd019fd81fa49
Adam Johnson <adam@adamjohnson.org>
2025-03-13 10:21:32 +1100

    More songs

--- components/home/content/Song.tsx
+++ components/home/content/Song.tsx
@@ -6,7 +6,7 @@ import * as Linking from "expo-linking";
 import FontAwesome6 from "@expo/vector-icons/FontAwesome6";
 import Animated, { Easing, useSharedValue, withTiming } from "react-native-reanimated";
 import SongsData from "data/songs.json";
-import { CheckInType } from "data/database";
+import { CheckInMoodType, CheckInType } from "data/database";
 import { theme, pressedDefault } from "utils/helpers";
 
 export type SongType = {
@@ -54,11 +54,79 @@ export default function Song(props: SongProps) {
     burninggold: require("../../../assets/img/music/burning-gold.png"),
     OneStepAtaTime: require("../../../assets/img/music/one-step-at-a-time.jpg"),
     FlyfeatRihanna: require("../../../assets/img/music/fly-feat-rihanna.jpg"),
+    Things: require("../../../assets/img/music/3-things.jpg"),
+    IAmLight: require("../../../assets/img/music/i-am-light.jpg"),
+    JusttheWayYouAre: require("../../../assets/img/music/just-the-way-you-are.jpg"),
+    LivingintheMoment: require("../../../assets/img/music/living-in-the-moment.jpg"),
+    WhatAWonderfulWorld: require("../../../assets/img/music/what-a-wonderful-world.jpg"),
+    CountonMe: require("../../../assets/img/music/count-on-me.png"),
+    MyWay: require("../../../assets/img/music/my-way.jpg"),
+    OneCallAway: require("../../../assets/img/music/one-call-away.png"),
+    AtTheRiver: require("../../../assets/img/music/at-the-river.jpg"),
+    Golden: require("../../../assets/img/music/golden.jpg"),
+    Easy: require("../../../assets/img/music/easy.jpg"),
+    Feeling: require("../../../assets/img/music/feeling.jpg"),
+    Beautiful: require("../../../assets/img/music/beautiful.png"),
+    TrueColors: require("../../../assets/img/music/true-colors.jpg"),
+    Try: require("../../../assets/img/music/try.jpg"),
+    human: require("../../../assets/img/music/human.png"),
+    Breathe2AM: require("../../../assets/img/music/breathe-2-am.png"),
+    RiseUp: require("../../../assets/img/music/rise-up.jpg"),
+    Angel: require("../../../assets/img/music/angel.png"),
+    FixYou: require("../../../assets/img/music/fix-you.png"),
+    Hands: require("../../../assets/img/music/hands.jpg"),
+    WhoYouAre: require("../../../assets/img/music/who-you-are.png"),
+    MadWorld: require("../../../assets/img/music/mad-world.jpg"),
+    AndSoItGoes: require("../../../assets/img/music/and-so-it-goes.jpg"),
+    WhatWasIMadeFor: require("../../../assets/img/music/what-was-i-made-for.png"),
+    colorblind: require("../../../assets/img/music/colorblind.jpg"),
+    Everglow: require("../../../assets/img/music/everglow.png"),
+    Landslide: require("../../../assets/img/music/landslide.jpg"),
+    SoI: require("../../../assets/img/music/so-i.png"),
+    breathin: require("../../../assets/img/music/breathin.png"),
+    Unwell: require("../../../assets/img/music/unwell.jpg"),
+    Years: require("../../../assets/img/music/7-years.jpg"),
+    Demons: require("../../../assets/img/music/demons.jpg"),
+    Rise: require("../../../assets/img/music/rise.png"),
+    Skyscraper: require("../../../assets/img/music/skyscraper.png"),
+    BadDay: require("../../../assets/img/music/bad-day.jpg"),
+    BoulevardofBrokenDreams: require("../../../assets/img/music/boulevard-of-broken-dreams.png"),
+    DarkSide: require("../../../assets/img/music/dark-side.jpg"),
+    EverybodyHurts: require("../../../assets/img/music/everybody-hurts.jpg"),
+    KingofAnything: require("../../../assets/img/music/king-of-anything.png"),
+    NoMoreDrama: require("../../../assets/img/music/no-more-drama.jpg"),
+    ScarsToYourBeautiful: require("../../../assets/img/music/scars-to-your-beautiful.png"),
+    StressedOut: require("../../../assets/img/music/stressed-out.png"),
+    Unsteady: require("../../../assets/img/music/unsteady.jpg"),
+    BehindBlueEyes: require("../../../assets/img/music/behind-blue-eyes.jpg"),
+    BreatheMe: require("../../../assets/img/music/breathe-me.jpg"),
+    IntheEnd: require("../../../assets/img/music/in-the-end.jpg"),
+    OKAnxietyAnthem: require("../../../assets/img/music/ok-anxiety-anthem.png"),
+    Numb: require("../../../assets/img/music/numb.jpg"),
+    HallofFamefeatwilliam: require("../../../assets/img/music/hall-of-fame-feat-william.jpg"),
+    NotAfraid: require("../../../assets/img/music/not-afraid.jpg"),
+    Superheroes: require("../../../assets/img/music/superheroes.png"),
+    TitaniumfeatSia: require("../../../assets/img/music/titanium-feat-sia.png"),
+    Confident: require("../../../assets/img/music/confident.png"),
+    EyeoftheTiger: require("../../../assets/img/music/eye-of-the-tiger.jpg"),
+    Radioactive: require("../../../assets/img/music/radioactive.jpg"),
+    Ride: require("../../../assets/img/music/ride.png"),
+    MyShot: require("../../../assets/img/music/my-shot.jpg"),
+    BreakingtheHabit: require("../../../assets/img/music/breaking-the-habit.jpg"),
+    Survivor: require("../../../assets/img/music/survivor.jpg"),
+    OneStepCloser: require("../../../assets/img/music/one-step-closer.jpg"),
+    BREAKMYSOUL: require("../../../assets/img/music/break-my-soul.png"),
   };
 
   useEffect(() => {
-    const random = SongsData[Math.floor(Math.random() * SongsData.length)]; // Random song
-    setSong(random);
+    const mood: CheckInMoodType = JSON.parse(props.checkIns[props.checkIns.length - 1].mood); // Latest check-in
+    const songs = SongsData.filter((item) => item.moods.includes(mood.color)); // Songs with check-in mood
+
+    if (songs.length) {
+      const random = songs[Math.floor(Math.random() * songs.length)]; // Random song
+      setSong(random);
+    }
+
     opacity.value = withTiming(1, { duration: 300, easing: Easing.in(Easing.cubic) });
   }, [JSON.stringify(props.checkIns)]);
 
@@ -115,6 +183,8 @@ export default function Song(props: SongProps) {
                 .replace(/\(/g, "")
                 .replace(/\)/g, "")
                 .replace(/\*/g, "")
+                .replace(/3/g, "")
+                .replace(/7/g, "")
                 .replace(/\!/g, "") as keyof typeof images
             ]
           }
