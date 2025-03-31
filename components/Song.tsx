32feadcd137b5fc6a18be9c1f130864b3e0cdf54
Adam Johnson <adam@adamjohnson.org>
2025-03-31 22:24:08 +1100

    Removed local content images

--- components/Song.tsx
+++ components/Song.tsx
@@ -29,94 +29,95 @@ export default function Song(props: SongProps) {
   const [song, setSong] = useState<SongType>();
   const spacing = Device.deviceType !== 1 ? 24 : 16;
   const iconSize = Device.deviceType !== 1 ? 32 : 24;
+  const url = "https://hvwvyufuezybxttghqiy.supabase.co/storage/v1/object/public/content/music/";
 
   const images = {
-    KeepYourHeadUp: require("../assets/img/music/keep-your-head-up.jpg"),
-    WakeMeUp: require("../assets/img/music/wake-me-up.png"),
-    Happy: require("../assets/img/music/happy.png"),
-    DontBeSoHardOnYourself: require("../assets/img/music/dont-be-so-hard-on-yourself.png"),
-    DontStopBelievin: require("../assets/img/music/dont-stop-believin.jpg"),
-    HeroeswecouldbeftToveLo: require("../assets/img/music/heroes-we-could-be-ft-tove-lo.jpg"),
-    Roar: require("../assets/img/music/roar.png"),
-    ShakeItOff: require("../assets/img/music/shake-it-off.png"),
-    BeOK: require("../assets/img/music/be-ok.png"),
-    RainOnMe: require("../assets/img/music/rain-on-me.png"),
-    BestDayOfMyLife: require("../assets/img/music/best-day-of-my-life.jpg"),
-    CANTSTOPTHEFEELING: require("../assets/img/music/cant-stop-the-feeling.png"),
-    GoodasHell: require("../assets/img/music/good-as-hell.jpg"),
-    Brave: require("../assets/img/music/brave.png"),
-    FightSong: require("../assets/img/music/fight-song.png"),
-    FkinPerfect: require("../assets/img/music/fkin-perfect.png"),
-    Say: require("../assets/img/music/say.png"),
-    ShakeItOut: require("../assets/img/music/shake-it-out.png"),
-    ILived: require("../assets/img/music/i-lived.png"),
-    TheClimb: require("../assets/img/music/the-climb.png"),
-    SomewhereOnlyWeKnow: require("../assets/img/music/somewhere-only-we-know.jpg"),
-    burninggold: require("../assets/img/music/burning-gold.png"),
-    OneStepAtaTime: require("../assets/img/music/one-step-at-a-time.jpg"),
-    FlyfeatRihanna: require("../assets/img/music/fly-feat-rihanna.jpg"),
-    Things: require("../assets/img/music/3-things.jpg"),
-    IAmLight: require("../assets/img/music/i-am-light.jpg"),
-    JusttheWayYouAre: require("../assets/img/music/just-the-way-you-are.jpg"),
-    LivingintheMoment: require("../assets/img/music/living-in-the-moment.jpg"),
-    WhatAWonderfulWorld: require("../assets/img/music/what-a-wonderful-world.jpg"),
-    CountonMe: require("../assets/img/music/count-on-me.png"),
-    MyWay: require("../assets/img/music/my-way.jpg"),
-    OneCallAway: require("../assets/img/music/one-call-away.png"),
-    AtTheRiver: require("../assets/img/music/at-the-river.jpg"),
-    Golden: require("../assets/img/music/golden.jpg"),
-    Easy: require("../assets/img/music/easy.jpg"),
-    Feeling: require("../assets/img/music/feeling.jpg"),
-    Beautiful: require("../assets/img/music/beautiful.png"),
-    TrueColors: require("../assets/img/music/true-colors.jpg"),
-    Try: require("../assets/img/music/try.jpg"),
-    human: require("../assets/img/music/human.png"),
-    Breathe2AM: require("../assets/img/music/breathe-2-am.png"),
-    RiseUp: require("../assets/img/music/rise-up.jpg"),
-    Angel: require("../assets/img/music/angel.png"),
-    FixYou: require("../assets/img/music/fix-you.png"),
-    Hands: require("../assets/img/music/hands.jpg"),
-    WhoYouAre: require("../assets/img/music/who-you-are.png"),
-    MadWorld: require("../assets/img/music/mad-world.jpg"),
-    AndSoItGoes: require("../assets/img/music/and-so-it-goes.jpg"),
-    WhatWasIMadeFor: require("../assets/img/music/what-was-i-made-for.png"),
-    colorblind: require("../assets/img/music/colorblind.jpg"),
-    Everglow: require("../assets/img/music/everglow.png"),
-    Landslide: require("../assets/img/music/landslide.jpg"),
-    SoI: require("../assets/img/music/so-i.png"),
-    breathin: require("../assets/img/music/breathin.png"),
-    Unwell: require("../assets/img/music/unwell.jpg"),
-    Years: require("../assets/img/music/7-years.jpg"),
-    Demons: require("../assets/img/music/demons.jpg"),
-    Rise: require("../assets/img/music/rise.png"),
-    Skyscraper: require("../assets/img/music/skyscraper.png"),
-    BadDay: require("../assets/img/music/bad-day.jpg"),
-    BoulevardofBrokenDreams: require("../assets/img/music/boulevard-of-broken-dreams.png"),
-    DarkSide: require("../assets/img/music/dark-side.jpg"),
-    EverybodyHurts: require("../assets/img/music/everybody-hurts.jpg"),
-    KingofAnything: require("../assets/img/music/king-of-anything.png"),
-    NoMoreDrama: require("../assets/img/music/no-more-drama.jpg"),
-    ScarsToYourBeautiful: require("../assets/img/music/scars-to-your-beautiful.png"),
-    StressedOut: require("../assets/img/music/stressed-out.png"),
-    Unsteady: require("../assets/img/music/unsteady.jpg"),
-    BehindBlueEyes: require("../assets/img/music/behind-blue-eyes.jpg"),
-    BreatheMe: require("../assets/img/music/breathe-me.jpg"),
-    IntheEnd: require("../assets/img/music/in-the-end.jpg"),
-    OKAnxietyAnthem: require("../assets/img/music/ok-anxiety-anthem.png"),
-    Numb: require("../assets/img/music/numb.jpg"),
-    HallofFamefeatwilliam: require("../assets/img/music/hall-of-fame-feat-william.jpg"),
-    NotAfraid: require("../assets/img/music/not-afraid.jpg"),
-    Superheroes: require("../assets/img/music/superheroes.png"),
-    TitaniumfeatSia: require("../assets/img/music/titanium-feat-sia.png"),
-    Confident: require("../assets/img/music/confident.png"),
-    EyeoftheTiger: require("../assets/img/music/eye-of-the-tiger.jpg"),
-    Radioactive: require("../assets/img/music/radioactive.jpg"),
-    Ride: require("../assets/img/music/ride.png"),
-    MyShot: require("../assets/img/music/my-shot.jpg"),
-    BreakingtheHabit: require("../assets/img/music/breaking-the-habit.jpg"),
-    Survivor: require("../assets/img/music/survivor.jpg"),
-    OneStepCloser: require("../assets/img/music/one-step-closer.jpg"),
-    BREAKMYSOUL: require("../assets/img/music/break-my-soul.png"),
+    KeepYourHeadUp: "keep-your-head-up.jpg",
+    WakeMeUp: "wake-me-up.png",
+    Happy: "happy.png",
+    DontBeSoHardOnYourself: "dont-be-so-hard-on-yourself.png",
+    DontStopBelievin: "dont-stop-believin.jpg",
+    HeroeswecouldbeftToveLo: "heroes-we-could-be-ft-tove-lo.jpg",
+    Roar: "roar.png",
+    ShakeItOff: "shake-it-off.png",
+    BeOK: "be-ok.png",
+    RainOnMe: "rain-on-me.png",
+    BestDayOfMyLife: "best-day-of-my-life.jpg",
+    CANTSTOPTHEFEELING: "cant-stop-the-feeling.png",
+    GoodasHell: "good-as-hell.jpg",
+    Brave: "brave.png",
+    FightSong: "fight-song.png",
+    FkinPerfect: "fkin-perfect.png",
+    Say: "say.png",
+    ShakeItOut: "shake-it-out.png",
+    ILived: "i-lived.png",
+    TheClimb: "the-climb.png",
+    SomewhereOnlyWeKnow: "somewhere-only-we-know.jpg",
+    burninggold: "burning-gold.png",
+    OneStepAtaTime: "one-step-at-a-time.jpg",
+    FlyfeatRihanna: "fly-feat-rihanna.jpg",
+    Things: "3-things.jpg",
+    IAmLight: "i-am-light.jpg",
+    JusttheWayYouAre: "just-the-way-you-are.jpg",
+    LivingintheMoment: "living-in-the-moment.jpg",
+    WhatAWonderfulWorld: "what-a-wonderful-world.jpg",
+    CountonMe: "count-on-me.png",
+    MyWay: "my-way.jpg",
+    OneCallAway: "one-call-away.png",
+    AtTheRiver: "at-the-river.jpg",
+    Golden: "golden.jpg",
+    Easy: "easy.jpg",
+    Feeling: "feeling.jpg",
+    Beautiful: "beautiful.png",
+    TrueColors: "true-colors.jpg",
+    Try: "try.jpg",
+    human: "human.png",
+    Breathe2AM: "breathe-2-am.png",
+    RiseUp: "rise-up.jpg",
+    Angel: "angel.png",
+    FixYou: "fix-you.png",
+    Hands: "hands.jpg",
+    WhoYouAre: "who-you-are.png",
+    MadWorld: "mad-world.jpg",
+    AndSoItGoes: "and-so-it-goes.jpg",
+    WhatWasIMadeFor: "what-was-i-made-for.png",
+    colorblind: "colorblind.jpg",
+    Everglow: "everglow.png",
+    Landslide: "landslide.jpg",
+    SoI: "so-i.png",
+    breathin: "breathin.png",
+    Unwell: "unwell.jpg",
+    Years: "7-years.jpg",
+    Demons: "demons.jpg",
+    Rise: "rise.png",
+    Skyscraper: "skyscraper.png",
+    BadDay: "bad-day.jpg",
+    BoulevardofBrokenDreams: "boulevard-of-broken-dreams.png",
+    DarkSide: "dark-side.jpg",
+    EverybodyHurts: "everybody-hurts.jpg",
+    KingofAnything: "king-of-anything.png",
+    NoMoreDrama: "no-more-drama.jpg",
+    ScarsToYourBeautiful: "scars-to-your-beautiful.png",
+    StressedOut: "stressed-out.png",
+    Unsteady: "unsteady.jpg",
+    BehindBlueEyes: "behind-blue-eyes.jpg",
+    BreatheMe: "breathe-me.jpg",
+    IntheEnd: "in-the-end.jpg",
+    OKAnxietyAnthem: "ok-anxiety-anthem.png",
+    Numb: "numb.jpg",
+    HallofFamefeatwilliam: "hall-of-fame-feat-william.jpg",
+    NotAfraid: "not-afraid.jpg",
+    Superheroes: "superheroes.png",
+    TitaniumfeatSia: "titanium-feat-sia.png",
+    Confident: "confident.png",
+    EyeoftheTiger: "eye-of-the-tiger.jpg",
+    Radioactive: "radioactive.jpg",
+    Ride: "ride.png",
+    MyShot: "my-shot.jpg",
+    BreakingtheHabit: "breaking-the-habit.jpg",
+    Survivor: "survivor.jpg",
+    OneStepCloser: "one-step-closer.jpg",
+    BREAKMYSOUL: "break-my-soul.png",
   };
 
   useEffect(() => {
@@ -179,22 +180,29 @@ export default function Song(props: SongProps) {
 
       <View style={{ flexDirection: "row", gap: spacing }}>
         <Image
-          source={
-            images[
-              song?.title
-                .replace(/ /g, "")
-                .replace(/\./g, "")
-                .replace(/\'/g, "")
-                .replace(/\(/g, "")
-                .replace(/\)/g, "")
-                .replace(/\*/g, "")
-                .replace(/3/g, "")
-                .replace(/7/g, "")
-                .replace(/\?/g, "")
-                .replace(/\!/g, "") as keyof typeof images
-            ]
-          }
-          style={{ width: Device.deviceType !== 1 ? 192 : 128, aspectRatio: "1/1", borderRadius: spacing / 2 }}
+          source={{
+            uri:
+              url +
+              images[
+                song?.title
+                  .replace(/ /g, "")
+                  .replace(/\./g, "")
+                  .replace(/\'/g, "")
+                  .replace(/\(/g, "")
+                  .replace(/\)/g, "")
+                  .replace(/\*/g, "")
+                  .replace(/3/g, "")
+                  .replace(/7/g, "")
+                  .replace(/\?/g, "")
+                  .replace(/\!/g, "") as keyof typeof images
+              ],
+          }}
+          style={{
+            width: Device.deviceType !== 1 ? 192 : 128,
+            aspectRatio: "1/1",
+            borderRadius: spacing / 2,
+            backgroundColor: colors.secondaryBg,
+          }}
         />
 
         <View style={{ flex: 1, gap: spacing }}>
