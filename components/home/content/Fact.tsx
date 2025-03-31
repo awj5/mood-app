bcb0284a3066aeec5ca7aef93372889e77474085
Adam Johnson <adam@adamjohnson.org>
2025-03-31 15:57:55 +1100

    Update Fact.tsx

--- components/home/content/Fact.tsx
+++ components/home/content/Fact.tsx
@@ -99,8 +99,8 @@ export default function Fact(props: FactProps) {
           style={{
             fontFamily: "Circular-Black",
             color: invertedColor,
-            fontSize: Device.deviceType !== 1 ? 24 : dimensions.width > 375 && fact.length <= 60 ? 18 : 16, // Smaller for iPhone SE
-            lineHeight: Device.deviceType !== 1 ? 28 : dimensions.width > 375 && fact.length <= 60 ? 20 : 18, // Smaller for iPhone SE
+            fontSize: Device.deviceType !== 1 ? 24 : dimensions.width > 375 && fact.length <= 56 ? 18 : 16, // Smaller for iPhone SE
+            lineHeight: Device.deviceType !== 1 ? 28 : dimensions.width > 375 && fact.length <= 56 ? 20 : 18, // Smaller for iPhone SE
           }}
           allowFontScaling={false}
         >
