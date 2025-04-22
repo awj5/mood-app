818ee0abdcd1102080225aaa4cd113bd0ff8f689
Adam Johnson <adam@adamjohnson.org>
2025-04-22 18:14:57 +1000

    Update Stats.tsx

--- components/home/content/Stats.tsx
+++ components/home/content/Stats.tsx
@@ -2,13 +2,14 @@ import { useContext, useEffect, useState } from "react";
 import { View, Text, StyleSheet, Pressable, PixelRatio } from "react-native";
 import * as Device from "expo-device";
 import { getLocales } from "expo-localization";
+import * as WebBrowser from "expo-web-browser";
 import Animated, { Easing, useSharedValue, withTiming } from "react-native-reanimated";
 import { Info } from "lucide-react-native";
-import { LineChart, lineDataItem } from "react-native-gifted-charts";
+import { BarChart, barDataItem } from "react-native-gifted-charts";
 import MoodsData from "data/moods.json";
-import { CheckInType, CheckInMoodType } from "data/database";
-import { DimensionsContext, DimensionsContextType } from "context/dimensions";
+import { CheckInMoodType, CheckInType } from "data/database";
 import { CalendarDatesType } from "context/home-dates";
+import { DimensionsContext, DimensionsContextType } from "context/dimensions";
 import { theme, pressedDefault } from "utils/helpers";
 
 type StatsProps = {
@@ -18,27 +19,22 @@ type StatsProps = {
 
 export default function Stats(props: StatsProps) {
   const colors = theme();
-  const fontScale = PixelRatio.getFontScale();
   const localization = getLocales();
+  const fontScale = PixelRatio.getFontScale();
   const opacity = useSharedValue(0);
   const { dimensions } = useContext<DimensionsContextType>(DimensionsContext);
-  const [satisfaction, setSatisfaction] = useState<lineDataItem[]>([]);
-  const [energy, setEnergy] = useState<lineDataItem[]>([]);
+  const [data, setData] = useState<barDataItem[]>([]);
   const spacing = Device.deviceType !== 1 ? 24 : 16;
-  const fontSize = Device.deviceType !== 1 ? 16 : 12;
-  const smallFont = Device.deviceType !== 1 ? 14 : 11;
-  const yAxisWidth = Device.deviceType !== 1 ? 52 : 40; // YAxis labels are 35 in width by default
-  const maxWidth = 720 + 48; // Max width of content wrapper
-  const rulesColor = colors.primary === "white" ? "rgba(255, 255, 255, 0.2)" : "rgba(0, 0, 0, 0.2)";
-  const invertedColor = colors.primary === "white" ? "black" : "white";
-  const dataPointSize = Device.deviceType !== 1 ? 7 : 5;
-  const grey = colors.primary === "white" ? "rgba(255,255,255,0.5)" : "rgba(0,0,0,0.5)";
-  const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-  const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+  const fontSize = Device.deviceType !== 1 ? 18 : 14;
+  const smallFont = Device.deviceType !== 1 ? 16 : 12;
+  const extraSmallFont = Device.deviceType !== 1 ? 14 : 10;
+  const tinyFont = Device.deviceType !== 1 ? 12 : 8;
+  const barSize = Device.deviceType !== 1 ? 15 : 10;
+  const days = ["S", "M", "T", "W", "T", "F", "S"];
+  const months = ["J", "F", "M", "A", "M", "J", "J", "A", "S", "O", "N", "D"];
 
   useEffect(() => {
-    const satisfactionData: lineDataItem[] = [];
-    const energyData: lineDataItem[] = [];
+    const dataItems: barDataItem[] = [];
     const dates: Date[] = [];
     let chartType = "week"; // week, days or months
     let start = new Date(); // Init
@@ -48,7 +44,7 @@ export default function Stats(props: StatsProps) {
       // Range
       start = new Date(props.dates.rangeStart);
       end = new Date(props.dates.rangeEnd);
-      const days = Math.ceil((end.getTime() - start.getTime()) / (1000 * 60 * 60 * 24)) + 1;
+      const days = Math.ceil((end.getTime() - start.getTime()) / (1000 * 60 * 60 * 24)) + 1; // Calculate no. of days
 
       if (days <= 62) {
         // Day view
@@ -79,24 +75,7 @@ export default function Stats(props: StatsProps) {
     for (let i = 0; i < dates.length; i++) {
       let date = dates[i];
 
-      // Add objects
-      satisfactionData.push({
-        label:
-          chartType === "months" && dates.length > 9 && dates.length <= 12
-            ? months[date.getMonth()].charAt(0)
-            : chartType === "months"
-            ? months[date.getMonth()]
-            : chartType === "days" && localization[0].languageTag === "en-US"
-            ? `${date.getMonth() + 1}/${date.getDate()}`
-            : chartType === "days"
-            ? `${date.getDate()}/${date.getMonth() + 1}`
-            : days[date.getDay()],
-        value: undefined,
-      });
-
-      energyData.push({ value: undefined });
-
-      // Get check-ins on label date
+      // Get date check-ins
       let checkIns = props.checkIns.filter((item) => {
         let itemDate = new Date(`${item.date}Z`); // Convert to UTC
         itemDate.setHours(0, 0, 0, 0);
@@ -109,66 +88,72 @@ export default function Stats(props: StatsProps) {
           : itemDate.getTime() === date.getTime();
       });
 
-      let satisfactionScores = [];
       let energyScores = [];
+      let stressScores = [];
 
-      // Loop check-ins and get mood satisfaction and energy scores
       for (let i = 0; i < checkIns.length; i++) {
         let mood: CheckInMoodType = JSON.parse(checkIns[i].mood);
-        satisfactionScores.push(MoodsData.filter((item) => item.id === mood.color)[0].stress);
         energyScores.push(MoodsData.filter((item) => item.id === mood.color)[0].energy);
+        stressScores.push(MoodsData.filter((item) => item.id === mood.color)[0].stress);
       }
 
-      if (satisfactionScores.length) {
-        // Calculate averages and update values
-        satisfactionData[i].value = Math.floor(
-          satisfactionScores.reduce((sum, num) => sum + num, 0) / satisfactionScores.length
-        );
+      // Energy
+      dataItems.push({
+        label:
+          chartType === "months"
+            ? months[date.getMonth()]
+            : chartType === "days" && localization[0].languageTag === "en-US"
+            ? `${date.getMonth() + 1}/${date.getDate()}`
+            : chartType === "days"
+            ? `${date.getDate()}/${date.getMonth() + 1}`
+            : days[date.getDay()],
+        value: energyScores.length
+          ? Math.floor(energyScores.reduce((sum, num) => sum + num, 0) / energyScores.length)
+          : 0,
+        spacing: spacing / 8,
+        frontColor: "white",
+        labelTextStyle: {
+          fontFamily: "Circular-Medium",
+          fontSize:
+            fontScale >= 1.2 && chartType === "days"
+              ? tinyFont
+              : fontScale >= 1.2 || chartType === "days"
+              ? extraSmallFont
+              : smallFont,
+          color: energyScores.length ? colors.primary : colors.opaque,
+        },
+        labelWidth: barSize * 2 + (spacing / 8) * 3,
+      });
 
-        energyData[i].value = Math.floor(energyScores.reduce((sum, num) => sum + num, 0) / energyScores.length);
-      }
+      dataItems.push({
+        value: stressScores.length
+          ? Math.floor(stressScores.reduce((sum, num) => sum + num, 0) / stressScores.length)
+          : 0,
+        frontColor: "black",
+      }); // Stress
     }
 
-    let delay = 0;
-
-    // Hack! - Add delay if data point spacing will change to avoid points stretching
-    if (
-      (satisfaction.length > 12 && satisfactionData.length <= 12) ||
-      (satisfaction.length <= 12 && satisfactionData.length > 12)
-    ) {
-      // Reset
-      setSatisfaction([]);
-      setEnergy([]);
-      delay = 100;
-    }
-
-    const timer = setTimeout(() => {
-      setSatisfaction(satisfactionData);
-      setEnergy(energyData);
-    }, delay);
-
+    setData(dataItems);
     opacity.value = withTiming(1, { duration: 300, easing: Easing.in(Easing.cubic) });
-    return () => clearTimeout(timer);
-  }, [props.checkIns]);
+  }, [props.checkIns, colors.primary]);
 
   return (
     <Animated.View
       style={{
         width: "100%",
-        backgroundColor: colors.primary === "white" ? "rgba(0,0,0,0.3)" : "rgba(255,255,255,0.4)",
+        backgroundColor: colors.opaqueBg,
         borderRadius: spacing,
         padding: spacing,
         opacity,
-        gap: spacing / 4,
       }}
     >
-      <View>
+      <View style={{ paddingBottom: spacing / 2 }}>
         <View style={{ flexDirection: "row", gap: spacing / 4 }}>
           <Text
             style={{
               fontFamily: "Circular-Bold",
               color: colors.primary,
-              fontSize: fontSize,
+              fontSize: smallFont,
             }}
             allowFontScaling={false}
           >
@@ -176,13 +161,12 @@ export default function Stats(props: StatsProps) {
           </Text>
 
           <Text
-            style={[
-              styles.beta,
-              {
-                color: colors.primary,
-                fontSize: Device.deviceType !== 1 ? 12 : 8,
-              },
-            ]}
+            style={{
+              fontFamily: "Circular-Book",
+              marginTop: Device.deviceType !== 1 ? -2 : -1,
+              color: colors.primary,
+              fontSize: Device.deviceType !== 1 ? 12 : 8,
+            }}
             allowFontScaling={false}
           >
             BETA
@@ -190,12 +174,12 @@ export default function Stats(props: StatsProps) {
         </View>
 
         <Pressable
-          onPress={() => alert("Coming soon")}
+          onPress={() => WebBrowser.openBrowserAsync("https://articles.mood.ai/mood-levels/")}
           style={({ pressed }) => [pressedDefault(pressed), styles.info, { gap: spacing / 4 }]}
           hitSlop={16}
         >
           <Info
-            color={grey}
+            color={colors.opaque}
             size={Device.deviceType !== 1 ? 20 : 16}
             absoluteStrokeWidth
             strokeWidth={Device.deviceType !== 1 ? 1.5 : 1}
@@ -204,8 +188,8 @@ export default function Stats(props: StatsProps) {
           <Text
             style={{
               fontFamily: "Circular-Book",
-              color: grey,
-              fontSize: fontSize,
+              color: colors.opaque,
+              fontSize: smallFont,
             }}
             allowFontScaling={false}
           >
@@ -214,114 +198,81 @@ export default function Stats(props: StatsProps) {
         </Pressable>
       </View>
 
-      <View style={{ gap: spacing / 2, overflow: "hidden" }}>
-        <LineChart
-          data={satisfaction}
-          data2={energy}
-          height={spacing * 4}
+      <View style={[styles.legend, { gap: spacing }]}>
+        <View style={[styles.key, { gap: spacing / 4 }]}>
+          <View
+            style={[
+              styles.dot,
+              {
+                backgroundColor: "white",
+                width: barSize,
+              },
+            ]}
+          />
+
+          <Text
+            style={{
+              fontFamily: "Circular-Medium",
+              color: colors.primary,
+              fontSize: fontSize,
+            }}
+            allowFontScaling={false}
+          >
+            Energy
+          </Text>
+        </View>
+
+        <View style={[styles.key, { gap: spacing / 4 }]}>
+          <View style={[styles.dot, { backgroundColor: "black", width: barSize }]} />
+
+          <Text
+            style={{
+              fontFamily: "Circular-Medium",
+              color: colors.primary,
+              fontSize: fontSize,
+            }}
+            allowFontScaling={false}
+          >
+            Stress
+          </Text>
+        </View>
+      </View>
+
+      <View style={{ overflow: "hidden" }}>
+        <BarChart
+          data={data}
           width={
-            dimensions.width > maxWidth
-              ? maxWidth - spacing * 4 - yAxisWidth
-              : dimensions.width - spacing * 4 - yAxisWidth
+            dimensions.width > dimensions.height
+              ? 768 - spacing * 4 - spacing * 2.5
+              : dimensions.width - spacing * 4 - spacing * 2.5
           }
-          endSpacing={spacing / 2} // Hack! - It seems the end spacing gets doubled
-          initialSpacing={spacing}
-          spacing={
-            satisfaction.length > 12
-              ? spacing * 2
-              : dimensions.width > maxWidth
-              ? (maxWidth - spacing * 4 - yAxisWidth - spacing * 2) / (satisfaction.length - 1)
-              : (dimensions.width - spacing * 4 - yAxisWidth - spacing * 2) / (satisfaction.length - 1)
-          }
-          noOfSections={2}
-          yAxisLabelSuffix="%"
-          roundToDigits={0}
-          maxValue={100}
-          yAxisExtraHeight={spacing}
-          overflowTop={spacing}
-          yAxisLabelWidth={yAxisWidth}
-          yAxisTextStyle={{
-            fontFamily: "Circular-Book",
-            fontSize: fontScale > 1 ? smallFont : fontSize,
-            color: colors.primary,
-            opacity: 0.5,
-          }}
-          xAxisLabelTextStyle={{
-            fontFamily: "Circular-Book",
-            fontSize: fontScale > 1 ? smallFont : fontSize,
-            color: colors.primary,
-            opacity: 0.5,
-          }}
+          height={Device.deviceType !== 1 ? 104 : 80}
+          barWidth={barSize}
+          roundedTop
+          roundedBottom
+          spacing={Device.deviceType !== 1 ? 48 : 16}
           yAxisThickness={0}
           xAxisThickness={0}
-          rulesColor={rulesColor}
-          rulesThickness={1}
-          showReferenceLine1
-          referenceLinesOverChartContent={false}
-          referenceLine1Position={0}
-          referenceLine1Config={{
-            color: rulesColor,
-            thickness: 1,
+          maxValue={100}
+          noOfSections={4}
+          yAxisLabelWidth={spacing * 2.5}
+          yAxisLabelSuffix="%"
+          yAxisTextStyle={{
+            fontFamily: "Circular-Book",
+            fontSize: fontScale >= 1.2 ? extraSmallFont : smallFont,
+            color: colors.opaque,
           }}
-          dataPointsColor1={colors.primary}
-          dataPointsColor2={invertedColor}
-          dataPointsRadius={dataPointSize}
-          color={colors.primary === "white" ? "rgba(255, 255, 255, 0.4)" : "rgba(0, 0, 0, 0.4)"}
-          color2={colors.primary === "white" ? "rgba(0, 0, 0, 0.4)" : "rgba(255, 255, 255, 0.4)"}
-          thickness={Device.deviceType !== 1 ? 6 : 4}
-          interpolateMissingValues={false}
-          disableScroll={satisfaction.length <= 12}
+          yAxisExtraHeight={spacing}
+          initialSpacing={spacing}
+          rulesColor={colors.primary === "white" ? "rgba(255,255,255,0.3)" : "rgba(0,0,0,0.2)"}
+          disablePress
         />
-
-        <View style={[styles.legend, { gap: spacing }]}>
-          <View style={[styles.key, { gap: spacing / 4 }]}>
-            <View style={[styles.dot, { backgroundColor: colors.primary, width: dataPointSize * 2 }]} />
-
-            <Text
-              style={{
-                fontFamily: "Circular-Medium",
-                color: colors.primary,
-                fontSize: fontSize,
-              }}
-              allowFontScaling={false}
-            >
-              Satisfaction
-            </Text>
-          </View>
-
-          <View style={[styles.key, { gap: spacing / 4 }]}>
-            <View
-              style={[
-                styles.dot,
-                {
-                  backgroundColor: invertedColor,
-                  width: dataPointSize * 2,
-                },
-              ]}
-            />
-
-            <Text
-              style={{
-                fontFamily: "Circular-Medium",
-                color: colors.primary,
-                fontSize: fontSize,
-              }}
-              allowFontScaling={false}
-            >
-              Energy
-            </Text>
-          </View>
-        </View>
       </View>
     </Animated.View>
   );
 }
 
 const styles = StyleSheet.create({
-  beta: {
-    fontFamily: "Circular-Book",
-    marginTop: 1,
-  },
   info: {
     position: "absolute",
     right: 0,
